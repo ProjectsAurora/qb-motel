d95912e1189d21b486cb68788dfd391a91388a18
Mobius <gmillustrated@gmail.com>
2024-09-18 20:19:27 -0700

    Convert to QBCORE
    Added Target
    Converted to QBCORE

--- client.lua
+++ client.lua
@@ -1,14 +1,13 @@
-local ESX = nil
+local QBCore = exports['qb-core']:GetCoreObject()
+
 Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-	ESX.PlayerData = ESX.GetPlayerData()
+    while QBCore == nil do
+        Citizen.Wait(0)
+    end
+    QBCore.PlayerData = QBCore.Functions.GetPlayerData()
 end)
 
 local currentmotel = nil
-local closestroom = nil
 local pinkcagecoord = vector3(326.747, -208.35, 54.0866)
 local pinkcage = {
     [1] = {door = vector3(307.57, -213.29, 54.22), h = 68.9096, doortext = vector3(307.318, -213.26, 54.2199), stash = vector3(306.71, -208.50, 54.22), obj = nil, clothe = vector3(302.58, -207.35, 54.22), locked = true},
@@ -30,19 +29,18 @@ local pinkcage = {
 local firstSpawn = true
 AddEventHandler('playerSpawned', function()
     if firstSpawn then
-        -- TriggerServerEvent('m3:motel:server:getLockStates')
         currentmotel = math.random(1, #pinkcage)
-        notify('inform', 'Yeni motel odası verildi! Oda numaran: '..currentmotel)
+        QBCore.Functions.Notify('You have been given a new motel room! Your room number is: '..currentmotel)
         firstSpawn = false
     end
 end)
 
-RegisterCommand('yenimotelodasi', function()
+RegisterCommand('newmotelroom', function()
     currentmotel = math.random(1, #pinkcage)
-    notify('inform', 'Yeni motel odası verildi! Oda numaran: '..currentmotel)
+    QBCore.Functions.Notify('You have been given a new motel room! Your room number is: '..currentmotel)
 end)
 
-RegisterCommand('motelcik', function()
+RegisterCommand('leavemotel', function()
     local player = PlayerPedId()
     local playercoords = GetEntityCoords(player)
     local moteldistance = #(playercoords - pinkcagecoord)
@@ -50,27 +48,15 @@ RegisterCommand('motelcik', function()
     if moteldistance <= 45.0 then
         SetEntityCoords(player, 311.491, -206.25, 58.0151, 0, 0, 0, 0)
         SetEntityHeading(player, 242.37)
-        notify('inform', 'Odadan ayrıldın.')
+        QBCore.Functions.Notify('You have left the room.')
     end
 end)
-
--- RegisterNetEvent('m3:motel:client:sendDoorlockState')
--- AddEventHandler('m3:motel:client:sendDoorlockState', function(doorlocktable)
---     for i=1, #pinkcage, 1 do
---         pinkcage[i].locked = doorlocktable[i].locked
---     end
--- end)
-
--- RegisterNetEvent('m3:motel:client:sendDoorlockState2')
--- AddEventHandler('m3:motel:client:sendDoorlockState2', function(doorid, lockstate)
---     pinkcage[doorid].locked = lockstate
--- end)
 
 Citizen.CreateThread(function()
     local gblip = AddBlipForCoord(pinkcagecoord)
     SetBlipSprite(gblip, 475)
     SetBlipDisplay(gblip, 4)
-    SetBlipScale (gblip, 0.6)
+    SetBlipScale(gblip, 0.6)
     SetBlipColour(gblip, 27)
     SetBlipAsShortRange(gblip, true)
     BeginTextCommandSetBlipName("STRING")
@@ -78,212 +64,119 @@ Citizen.CreateThread(function()
     EndTextCommandSetBlipName(gblip)
 end)
 
--- Citizen.CreateThread(function()
---     while true do
---         local player = PlayerPedId()
---         local playercoords = GetEntityCoords(player)
---         local moteldistance = #(playercoords - pinkcagecoord)
-
---         if moteldistance <= 45.0 then
---             for j = 1, #pinkcage, 1 do
---                 local doordistance = #(playercoords - pinkcage[j].door)
---                 if pinkcage[j].obj == nil or not DoesEntityExist(pinkcage[j].obj) then
---                     pinkcage[j].obj = GetClosestObjectOfType(pinkcage[j].door, 1.2, -1156992775, false, false, false) ---1156992775 door hash
---                     FreezeEntityPosition(pinkcage[j].obj, pinkcage[j].locked)
---                 else
---                     FreezeEntityPosition(pinkcage[j].obj, pinkcage[j].locked)
---                     if pinkcage[j].locked then
---                         SetEntityHeading(pinkcage[j].obj, pinkcage[j].h)
---                     end
---                 end
---                 if doordistance <= 30.0 and currentmotel == j then
---                     DrawMarker(2, pinkcage[j].doortext.x, pinkcage[j].doortext.y, pinkcage[j].doortext.z - 0.3, 0, 0, 0, 0, 0, 0, 0.2, 0.2, 0.2, 32, 236, 54, 100, 0, 0, 0, 1, 0, 0, 0)
---                 end
---                 if doordistance <= 2.0 then
---                     if pinkcage[j].locked and currentmotel == j then 
---                         closestroom = j
---                         closestString = "[Oda: "..j.."] - [E] - Kilitli"
---                         DrawText3D(pinkcage[j].doortext, closestString) 
---                     elseif not pinkcage[j].locked and currentmotel == j then 
---                         closestString = "[Oda: "..j.."] - [E] - Açık"
---                         DrawText3D(pinkcage[j].doortext, closestString) 
---                     elseif pinkcage[j].locked and currentmotel ~= j then
---                         closestString = "[Oda: "..j.."] - Kilitli"
---                         DrawText3D(pinkcage[j].doortext, closestString) 
---                     elseif not pinkcage[j].locked and currentmotel ~= j then
---                         closestString = "[Oda: "..j.."] - Açık"
---                         DrawText3D(pinkcage[j].doortext, closestString)
---                     end
---                     if (doordistance <= 1.5 and currentmotel == j) then
---                         if IsControlJustReleased(0, 38) then
---                             TriggerServerEvent('m3:motel:server:toggleDoorlock', j, not pinkcage[j].locked)
---                             openHouseAnim()
---                         end
---                     end
---                 end
---                 -- Citizen.Wait(0)
---             end
---         else
---             Citizen.Wait(500)
---         end
---         Citizen.Wait(5)
---     end
--- end)
-
+-- Target system integration for doors, stash, and wardrobe using qb-target
 Citizen.CreateThread(function()
-    while true do
-        if currentmotel ~= nil then
-            local player = PlayerPedId()
-            local playercoords = GetEntityCoords(player)
-            local stashdistance = #(playercoords - pinkcage[currentmotel].stash)
-            local clothedistance = #(playercoords - pinkcage[currentmotel].clothe)
-            local doordistance = #(playercoords - pinkcage[currentmotel].doortext)
-            local moteldistance = #(playercoords - pinkcagecoord)
+    for k, v in pairs(pinkcage) do
+        -- Target interaction for doors
+        exports['qb-target']:AddBoxZone("MotelDoor_"..k, v.doortext, 1.5, 1.5, {
+            name = "MotelDoor_"..k,
+            heading = v.h,
+            debugPoly = false,
+            minZ = v.doortext.z - 1,
+            maxZ = v.doortext.z + 1
+        }, {
+            options = {
+                {
+                    type = "client",
+                    event = 'm3:motel:client:toggleDoor',
+                    icon = "fas fa-door-open",
+                    label = "Open/Close Door",
+                    room = k,
+                }
+            },
+            distance = 2.0
+        })
 
-            if moteldistance <= 45.0 then
-                if doordistance <= 30.0 then
-                    DrawMarker(2, pinkcage[currentmotel].doortext.x, pinkcage[currentmotel].doortext.y, pinkcage[currentmotel].doortext.z - 0.3, 0, 0, 0, 0, 0, 0, 0.2, 0.2, 0.2, 32, 236, 54, 100, 0, 0, 0, 1, 0, 0, 0)
-                end
+        -- Target interaction for stash
+        exports['qb-target']:AddBoxZone("MotelStash_"..k, v.stash, 1.5, 1.5, {
+            name = "MotelStash_"..k,
+            heading = v.h,
+            debugPoly = false,
+            minZ = v.stash.z - 1,
+            maxZ = v.stash.z + 1
+        }, {
+            options = {
+                {
+                    type = "client",
+                    event = 'm3:motel:client:openStash',
+                    icon = "fas fa-box",
+                    label = "Open Stash",
+                    room = k,
+                }
+            },
+            distance = 1.5
+        })
 
-                -- if stashdistance <= 3.0 then
-                    -- DrawMarker(2, pinkcage[currentmotel].stash.x, pinkcage[currentmotel].stash.y, pinkcage[currentmotel].stash.z - 0.3, 0, 0, 0, 0, 0, 0, 0.1, 0.1, 0.1, 32, 236, 54, 100, 0, 0, 0, 1, 0, 0, 0)
-                    if stashdistance <= 1.5 then
-                        DrawText3D(pinkcage[currentmotel].stash, '[~g~E~w~] - Sandık')
-                        if IsControlJustReleased(0, 38) then
-                            OpenMotelInventory()
-                        end
-                    end
-                -- end
-                -- if clothedistance <= 3.0 then
-                    -- DrawMarker(2, pinkcage[currentmotel].clothe.x, pinkcage[currentmotel].clothe.y, pinkcage[currentmotel].clothe.z - 0.3, 0, 0, 0, 0, 0, 0, 0.1, 0.1, 0.1, 32, 236, 54, 100, 0, 0, 0, 1, 0, 0, 0)
-                    if clothedistance <= 1.5 then
-                        DrawText3D(pinkcage[currentmotel].clothe, '[~g~E~w~] - Gardrop')
-                        if IsControlJustReleased(0, 38) then
-                            OpenMotelWardrobe()
-                        end
-                    end
-                -- end
-            else
-                Citizen.Wait(500)
-            end
-        end
-        Citizen.Wait(5)
+        -- Target interaction for wardrobe
+        exports['qb-target']:AddBoxZone("MotelWardrobe_"..k, v.clothe, 1.5, 1.5, {
+            name = "MotelWardrobe_"..k,
+            heading = v.h,
+            debugPoly = false,
+            minZ = v.clothe.z - 1,
+            maxZ = v.clothe.z + 1
+        }, {
+            options = {
+                {
+                    type = "client",
+                    event = 'm3:motel:client:openWardrobe',
+                    icon = "fas fa-tshirt",
+                    label = "Open Wardrobe",
+                    room = k,
+                }
+            },
+            distance = 1.5
+        })
     end
 end)
 
-function openHouseAnim()
-    loadAnimDict("anim@heists@keycard@") 
-    TaskPlayAnim(PlayerPedId(), "anim@heists@keycard@", "exit", 5.0, 1.0, -1, 16, 0, 0, 0, 0 )
-    Citizen.Wait(400)
-    ClearPedTasks(PlayerPedId())
-end
+-- Toggle door lock event
+RegisterNetEvent('m3:motel:client:toggleDoor')
+AddEventHandler('m3:motel:client:toggleDoor', function(data)
+    local room = data.room
+    local locked = pinkcage[room].locked
+    pinkcage[room].locked = not locked
+    TriggerServerEvent('m3:motel:server:toggleDoorlock', room, not locked)
+end)
 
-function loadAnimDict(dict)
-    RequestAnimDict(dict)
-    while not HasAnimDictLoaded(dict) do
-        Wait(5)
-    end
-end
+-- Open motel wardrobe using qb-clothing
+RegisterNetEvent('m3:motel:client:openWardrobe')
+AddEventHandler('m3:motel:client:openWardrobe', function()
+    TriggerEvent('qb-clothing:client:openOutfitMenu')
+end)
 
-function OpenMotelWardrobe()
-    ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'room',{
-		title    = 'Gardrop',
-		align    = 'right',
-		elements = {
-            {label = 'Kıyafetler', value = 'player_dressing'},
-	        {label = 'Kıyafet Sil', value = 'remove_cloth'}
-        }
-	}, function(data, menu)
+-- Open motel stash
+RegisterNetEvent('m3:motel:client:openStash')
+AddEventHandler('m3:motel:client:openStash', function(data)
+    local room = data.room
+    TriggerEvent('inventory:client:SetCurrentStash', 'MotelRoom_'..room)
+    TriggerServerEvent('inventory:server:OpenInventory', 'stash', 'MotelRoom_'..room, {
+        maxweight = 1000000,
+        slots = 50,
+    })
+end)
 
-		if data.current.value == 'player_dressing' then 
-            menu.close()
-			ESX.TriggerServerCallback('m3:motel:server:getPlayerDressing', function(dressing)
-				elements = {}
-
-				for i=1, #dressing, 1 do
-					table.insert(elements, {
-						label = dressing[i],
-						value = i
-					})
-				end
-
-				ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'player_dressing',
-				{
-					title    = 'Kıyafetler',
-					align    = 'right',
-					elements = elements
-				}, function(data2, menu2)
-
-					TriggerEvent('skinchanger:getSkin', function(skin)
-						ESX.TriggerServerCallback('m3:motel:server:getPlayerOutfit', function(clothes)
-							TriggerEvent('skinchanger:loadClothes', skin, clothes)
-							TriggerEvent('esx_skin:setLastSkin', skin)
-
-							TriggerEvent('skinchanger:getSkin', function(skin)
-								TriggerServerEvent('esx_skin:save', skin)
-							end)
-						end, data2.current.value)
-					end)
-
-				end, function(data2, menu2)
-					menu2.close()
-				end)
-			end)
-
-		elseif data.current.value == 'remove_cloth' then
-            menu.close()
-			ESX.TriggerServerCallback('m3:motel:server:getPlayerDressing', function(dressing)
-				elements = {}
-
-				for i=1, #dressing, 1 do
-					table.insert(elements, {
-						label = dressing[i],
-						value = i
-					})
-				end
-
-				ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'remove_cloth', {
-					title    = 'Kıyafet Sil',
-					align    = 'right',
-					elements = elements
-				}, function(data2, menu2)
-					menu2.close()
-					TriggerServerEvent('m3:motel:server:removeOutfit', data2.current.value)
-                    TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', text = 'Kıyafet silindi!'})
-				end, function(data2, menu2)
-					menu2.close()
-				end)
-			end)
-		end
-	end, function(data, menu)
-        menu.close()
-	end)
-end
-
-function OpenMotelInventory()
-    TriggerEvent('m3:inventoryhud:client:openStash', 'Pinkcage Motel', 'all')
-end
-
+-- Function to notify players
 function notify(type, text, time)
     if length == nil then length = 5000 end 
-    TriggerEvent('mythic_notify:client:SendAlert', { type = type, text = text, length = length})
+    QBCore.Functions.Notify(text, type)
 end
 
+-- Draw 3D text utility
 function DrawText3D(coord, text)
-	local onScreen,_x,_y=GetScreenCoordFromWorldCoord(coord.x, coord.y, coord.z)
-	local px,py,pz=table.unpack(GetGameplayCamCoords()) 
-	local scale = 0.3
-	if onScreen then
-		SetTextScale(scale, scale)
-		SetTextFont(4)
-		SetTextProportional(1)
-		SetTextColour(255, 255, 255, 215)
-		SetTextDropshadow(0)
-		SetTextEntry("STRING")
-		SetTextCentre(1)
-		AddTextComponentString(text)
-        DrawText(_x,_y)
+    local onScreen, _x, _y = GetScreenCoordFromWorldCoord(coord.x, coord.y, coord.z)
+    local px, py, pz = table.unpack(GetGameplayCamCoords())
+    local scale = 0.3
+    if onScreen then
+        SetTextScale(scale, scale)
+        SetTextFont(4)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 215)
+        SetTextDropshadow(0)
+        SetTextEntry("STRING")
+        SetTextCentre(1)
+        AddTextComponentString(text)
+        DrawText(_x, _y)
         local factor = (string.len(text)) / 380
         DrawRect(_x, _y + 0.0120, 0.0 + factor, 0.025, 41, 11, 41, 100)
-	end
+    end
 end
